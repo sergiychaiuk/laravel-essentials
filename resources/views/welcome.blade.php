e4d1defc4d81e67829b2a44cac09c5c219547f94
serhiichayck <chayck7@gmail.com>
2021-03-14 13:25:57 +0200

    Configuration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ App::environment() }}
                 </div>
 
                 <div class="links">
