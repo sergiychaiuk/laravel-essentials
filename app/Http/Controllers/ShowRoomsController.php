632f166136e5fd6b566f51f52ccb6ffc5e6c13cb
serhiichayck <chayck7@gmail.com>
2021-03-15 21:57:29 +0200

    The first view

--- app/Http/Controllers/ShowRoomsController.php
+++ app/Http/Controllers/ShowRoomsController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
 
 class ShowRoomsController extends Controller
 {
@@ -13,7 +16,7 @@ class ShowRoomsController extends Controller
      * Handle the incoming request.
      *
      * @param Request $request
-     * @return JsonResponse|Response
+     * @return Application|Factory|JsonResponse|Response|View
      */
     public function __invoke(Request $request)
     {
@@ -21,6 +24,6 @@ class ShowRoomsController extends Controller
         if ($request->query('id') !== null) {
             $rooms = $rooms->where('room_type_id', $request->query('id'));
         }
-        return response()->json($rooms);
+        return view('rooms.index', ['rooms' => $rooms]);
     }
 }
