9f264e0bcb2e8a2ebf93419813e5764eb9a4d59b
serhiichayck <chayck7@gmail.com>
2021-03-17 19:46:03 +0200

    Add saving

--- app/Booking.php
+++ app/Booking.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    protected $fillable = [
+        'room_id',
+        'start',
+        'end',
+        'is_reservation',
+        'is_paid',
+        'notes',
+    ];
 }
