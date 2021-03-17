501ec5c5c72406d49f7cfba721278560cca88d82
serhiichayck <chayck7@gmail.com>
2021-03-17 21:10:10 +0200

    Add soft-deleting

--- app/Booking.php
+++ app/Booking.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Booking extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'room_id',
         'start',
