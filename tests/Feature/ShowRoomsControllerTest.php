f4acbbb860de59a34c74383f94ee58fb70ef8d8a
serhiichayck <chayck7@gmail.com>
2021-03-28 01:20:17 +0200

    Create database factories

--- tests/Feature/ShowRoomsControllerTest.php
+++ tests/Feature/ShowRoomsControllerTest.php
@@ -3,9 +3,12 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ShowRoomsControllerTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
@@ -19,5 +22,19 @@ class ShowRoomsControllerTest extends TestCase
         ->assertSeeText('Type')
         ->assertViewIs('rooms.index')
         ->assertViewHas('rooms');
+    }
+
+    public function testRoomParameter() {
+        $roomTypes = factory('App\RoomType', 3)->create();
+        $rooms = factory('App\Room', 20)->create();
+        $roomType = $roomTypes->random();
+
+        $response = $this->get('/rooms/' . $roomType->id);
+
+        $response->assertStatus(200)
+            ->assertSeeText('Type')
+            ->assertViewIs('rooms.index')
+            ->assertViewHas('rooms')
+            ->assertSeeText($roomType->name);
     }
 }
