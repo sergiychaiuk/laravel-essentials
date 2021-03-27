e5c472a3a16198906360ae4656a7a9c1623342b8
serhiichayck <chayck7@gmail.com>
2021-03-28 01:38:09 +0200

    Test as a user

---
+++ tests/Feature/HomeControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class HomeControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $user = factory('App\User')->create();
+        $response = $this->actingAs($user)->get('/home');
+
+        $response->assertStatus(200);
+    }
+
+    public function testLoggedOut()
+    {
+        $response = $this->get('/home');
+
+        $response->assertStatus(302)
+            ->assertRedirect('/login');
+    }
+}
