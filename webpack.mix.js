38cc82b8d5865fc2f0dc0881644cb58635ed1f5a
serhiichayck <chayck7@gmail.com>
2021-03-30 21:20:42 +0300

    CSS and JS processing

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,17 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .sourceMaps()
+    .extract(['vue'])
+    .version()
     .sass('resources/sass/app.scss', 'public/css');
+
+// mix.browserSync('sync.test');
+//mix.scripts(['first.js', 'second.js'], 'all.js');
+// mix.babel(['first.js', 'second.js'], 'all.js');
+// mix.react('app.jsx', 'public/js');
+
+// mix.less('path.less', 'public/css/less.css')
+//     .less('path2.less', 'public/css/less2.css');
+//
+// mix.styles(['first.css', 'second.css'], 'public/css/all.css');
