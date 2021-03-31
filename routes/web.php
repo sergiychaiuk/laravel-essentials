e19332965c70ac25f87e882a38fd40665cac3cfc
serhiichayck <chayck7@gmail.com>
2021-03-31 22:25:04 +0300

    Test hash

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Support\Facades\Hash;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,7 +20,20 @@ Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/test', function () { return "Goodbye"; })->middleware('verified');
+Route::get('/test', function () {
+    //$secret = encrypt('black-saber');
+    //var_dump($secret);
+    //var_dump(decrypt($secret));
+    $hash = Hash::make('black-saber');
+    var_dump($hash);
+    var_dump(Hash::check('black-saber', $hash));
+    var_dump(Hash::needsRehash($hash));
+    $oldHash = Hash::make('black-saber', ['rounds' => 5]);
+    var_dump($oldHash);
+    var_dump(Hash::needsRehash($oldHash));
+    var_dump(Hash::make('black-saber'));
+})->middleware('verified');
+//Route::get('/test', function () { return "Goodbye"; })->middleware('verified');
 //Route::get('/test', function () { return "Goodbye"; })->middleware('\App\Http\Middleware\CheckQueryParam');
 
 //Route::get('/rooms/{name}/{roomType?}', 'ShowRoomsController')->where('name', '[A-Za-z]+');
